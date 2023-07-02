967d8d58379b859cf0c77bff83e05cdb606a4f8b
Romulo Americo <116516646+Romulo-Americo@users.noreply.github.com>
2023-07-02 16:38:48 -0300

    Add files via upload

---
+++ metodos_string.js
@@ -0,0 +1,19 @@
+var nome = "romulo";
+//conta as letras da string
+console.log(nome.length);
+//coloca a string toda em maiuscula
+console.log(nome.toLocaleUpperCase);
+//coloca a string toda em minuscula)
+console.log(nome.toLocaleLowerCase);
+//procura uma string especificada no programa
+console.log(nome.indexOf("O"));
+//manipula onde queremos que a string seja lida ou feita algo
+console.log(nome.slice(0,3));
+//Procura uma palavra na string resultando em verdadeiro ou falso
+console.logo(nome.includes(xxx));
+//Concatenação de strings
+console.log(nome.concat(variavelx));
+//remove os espaços no começo e no fim de uma strign
+console.log(nome.trim());
+//remove uma string da variavel
+console.log(nome.split("m"));
